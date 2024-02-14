e5d49f74cae6cb5cf1c18ec9ae35197f7edbc87c
Masum Rana <masumrana446267@gmail.com>
2024-02-14 17:26:24 +0600

    <comment_reaction> comment reaction implement done

---
+++ src/app/modules/commentReaction/comment_reaction.route.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import validateRequest from '../../middlewares/ValidateRequest';
+import { ENUM_USER_ROLE } from '../../Enum/role';
+import auth from '../../middlewares/auth';
+import { CommentreactionValidationSchema } from './comment_reaction.validation';
+import { Comment_ReactionController } from './comment_reaction.controller';
+
+const router = express.Router();
+
+router.post(
+  '/',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.NORMAL_USER),
+  validateRequest(CommentreactionValidationSchema.reactionZodSchema),
+  Comment_ReactionController.makeAndUndoReaction,
+);
+
+router.get(
+  '/:postid',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.NORMAL_USER),
+  ReactionController.getAllReaction,
+);
+
+export const ReactionRoutes = router;
