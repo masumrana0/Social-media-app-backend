29b899f7d0d66a5288eb9ce809fede6676997a38
Masum Rana <masumrana446267@gmail.com>
2024-02-17 21:37:04 +0600

    <story> get story service created done

--- src/app/modules/story/story.service.ts
+++ src/app/modules/story/story.service.ts
@@ -6,6 +6,12 @@ const createStory = async (payload: IStory): Promise<IStory | null> => {
   return result;
 };
 
+const getAllStory = async (payload: IStory): Promise<IStory | null> => {
+  const result = await Story.create(payload);
+  return result;
+};
+
 export const StoryService = {
   createStory,
+  getAllStory,
 };
